5c0f70ca53caf172a89295fe5080a257f0d68edd
Kirkland03 <44795796+Kirkland03@users.noreply.github.com>
2022-06-08 15:13:38 -0700

    Made several bug fixes

--- Design/datapath_all.sv
+++ Design/datapath_all.sv
@@ -1,11 +1,11 @@
 //DMA Datapath
-module Datapath ( output logic [7:0] CommandRegOut, //Output from Command Register to Priority Block
-				  output wire  [7:0] Address,		//Output from Temporary Address Regsister to external A7-A0 pins	
-				  output logic [3:0] MaskRegOut,	//Output from Mask Register to Priority Block  	 	
-				  output logic [7:2] ModeRegOut[3:0],  //Output from Mode Registers to Control Block	
-				  inout  wire  [7:0] DB, 			//InOut  to CPU Databus, DB7-DB0
-				  input  logic [3:0] PendingReq,    //Pending DMA Requests by Channel from Priority Block
-				  ControlIF Ctrl,			   		//Control interface from Control block to Registers	
+module Datapath ( output logic [7:0] CommandRegOut,    //Output from Command Register to Priority Block
+				  output wire  [7:0] Address,		   //Output from Temporary Address Regsister to external A7-A0 pins	
+				  output logic [3:0] MaskRegOut,	   //Output from Mask Register to Priority Block  	 	
+				  output logic [7:2] ModeRegOut[4],    //Output from Mode Registers to Control Block	
+				  inout  wire  [7:0] DB, 			   //InOut  to CPU Databus, DB7-DB0
+				  input  logic [3:0] PendingReq,       //Pending DMA Requests by Channel from Priority Block
+				  ControlIF.Datapath Ctrl,			   //Control interface from Control block to Registers,
 				  input  logic Clock, Reset);
 				  
 //Register Abbreviations: 
@@ -20,8 +20,9 @@ logic [15:0] IncOut[3:0], DecOut[3:0];  //Incrementor and decrementor outputs
 logic [15:0] Mux2CAR[3:0], Mux2TAR[3:0], Mux2CWC[3:0], Mux2TWC[3:0]; 
 bit LOW = 1'b0;
 wire [7:0] IntAddress; //internal address lines: IntAddress[7:0] to buffer and out to pins
+logic [3:0] TerminalCount;
 
-	IntBus Bus(.*);	//Internal Data Bus			  
+	IntBus Bus(.*);	//Internal Data Bus				  
 
 //Register Instantiations.
 //Instance names will be Register[i].Command, Register[i].Mask, etc.
@@ -32,16 +33,16 @@ generate begin: Register //Couldnt add Register here as well as below
 	
 	MaskReg    Mask   (Bus.MaskMod, MaskRegOut, Ctrl.ClearMask, Ctrl.WriteAllMask, Ctrl.MaskLoad, Ctrl.MasterClear);
 	
-	StatusReg  Status (Bus.StatusMod, Ctrl.StatusEnable, Ctrl.MasterClear);
+	StatusReg  Status (Bus.StatusMod, PendingReq, TerminalCount,  Ctrl.StatusEnable, Ctrl.MasterClear);
 
 	AddressBuff AddrBuffer(Address[7:0], IntAddress, Ctrl.AddrBufEnbable);
 	
-	DataIOBuff DataBuffer (Bus.DataBufMod, DB,  Ctrl.DataBufEnable );
+	DataIOBuff DataBuffer (Bus.DataBufMod, DB,  Ctrl.DataBufEnable, Ctrl.DataBufLoad);
 
-for(i = 0; i < 4; i++) begin
+for(i = 0; i < 4; i++) begin:i
 	//Create Bank of 4 Address Registers
-	BaseAddressReg    BaseAddress    ( Bus.BARMod, 			  			  			   Ctrl.BARLoad[i], Ctrl.FF);
-	CurrentAddressReg CurrentAddress ( Bus.CARMod,     		       		   Mux2CAR[i], Ctrl.CAREnable[i], Ctrl.CARLoad[i], Ctrl.FF, Ctrl.AddrMuxSel[i]);
+	BaseAddressReg    BaseAddress    ( Bus.BARMod, Ctrl.BARLoad[i], Ctrl.FF);
+	CurrentAddressReg CurrentAddress ( Bus.CARMod, Mux2CAR[i], Ctrl.CAREnable[i], Ctrl.CARLoad[i], Ctrl.FF, Ctrl.AddrMuxSel[i]);
 	TempAddressReg	   TempAddress   ( Bus.TARMod, TAROut[i], IntAddress,  Mux2TAR[i], Ctrl.TAREnbale[i], Ctrl.TARLoad[i], Ctrl.FF, Ctrl.AddrMuxSel[i]);
 	
 	Mux2to1 CARMux( Mux2CAR[i], Bus.Data, TAROut[i], Ctrl.AddrMuxSel[i]); 
@@ -49,9 +50,9 @@ for(i = 0; i < 4; i++) begin
 	IncDec Incrementor( IncOut[i], TAROut[i], ModeRegOut[i][5]); //ModeReg Bit 5 sets whether Address is Incremented/Decremented
 	
 	//Create Bank of 4 WordCount Registers
-	BaseWordCountReg 	BaseWordCount    ( Bus.BWCMod, 						   Ctrl.BWCLoad[i], Ctrl.FF);
-	CurrentWordCountReg CurrentWordCount ( Bus.CWCMod, 			   Mux2CWC[i], Ctrl.CWCEnable[i], Ctrl.CWCLoad[i], Ctrl.FF, Ctrl.WCMuxSel[i]);				
-	TempWordCountReg	TempWordCount    ( Bus.TWCMod,  TWCOut[i], Mux2TWC[i], Ctrl.TWCEnable[i], Ctrl.TWCLoad[i], Ctrl.FF, Ctrl.WCMuxSel[i]);
+	BaseWordCountReg 	BaseWordCount    ( Bus.BWCMod, Ctrl.BWCLoad[i], Ctrl.FF);
+	CurrentWordCountReg CurrentWordCount ( Bus.CWCMod, Mux2CWC[i], Ctrl.CWCEnable[i], Ctrl.CWCLoad[i], Ctrl.FF, Ctrl.WCMuxSel[i]);				
+	TempWordCountReg	TempWordCount    ( Bus.TWCMod, TWCOut[i], TerminalCount[i], Mux2TWC[i], Ctrl.TWCEnable[i], Ctrl.TWCLoad[i], Ctrl.FF, Ctrl.WCMuxSel[i]);
 			  
 	Mux2to1 CWCMux( Mux2CWC[i], Bus.Data, TWCOut[i], Ctrl.WCMuxSel[i]);
 	Mux2to1 TWCMux( Mux2TWC[i], Bus.Data, DecOut[i], Ctrl.WCMuxSel[i]);
@@ -81,9 +82,10 @@ module CurrentAddressReg ( IntBus.CARMod Bus,
 
 	always @(posedge Bus.Clock) 
 		if (Bus.Reset) CAddress <= '0;
-		else if (Load && SEL && FF) CAddress[15:8] <= CAIn[15:8];
+		else if (Load && SEL && FF) CAddress[15:8] <= CAIn[7:0];
 		else if (Load && SEL)       CAddress[7:0]  <= CAIn[7:0];
-		else if (Load) 			    CAddress 	   <= CAIn; 
+		else if (Load) 			    CAddress 	   <= CAIn;
+		else 						CAddress       <= CAddress;
 				   
 endmodule
 
@@ -103,9 +105,10 @@ module CurrentWordCountReg ( IntBus.CWCMod Bus,
 
 	always @(posedge Bus.Clock)
 		if (Bus.Reset) CWordCount <= '0;
-		else if (Load && SEL && FF) CWordCount[15:8] <= CWCIn[15:8];
+		else if (Load && SEL && FF) CWordCount[15:8] <= CWCIn[7:0];
 		else if (Load && SEL )		CWordCount[7:0]  <= CWCIn[7:0];
-		else if (Load)		        CWordCount       <= CWCIn;	
+		else if (Load)		        CWordCount       <= CWCIn;
+		else         		        CWordCount       <= CWordCount;			
 		
 endmodule
 
@@ -131,9 +134,10 @@ module TempAddressReg( IntBus.TARMod Bus,       //Output to Internal Data Bus
 
 	always @(posedge Bus.Clock)
 		if (Bus.Reset) TAddress <= '0;
-		else if (Load && SEL && FF) TAddress[15:8] <= TARIn[15:8];
+		else if (Load && SEL && FF) TAddress[15:8] <= TARIn[7:0];
 		else if (Load && SEL) 	    TAddress[7:0]  <= TARIn[7:0];
 		else if (Load)				TAddress       <= TARIn;
+		else 						TAddress	   <= TAddress;
 			  
 endmodule
 
@@ -157,9 +161,10 @@ module TempWordCountReg( IntBus.TWCMod Bus,   	  	  //Output to Internal Data Bu
 
 	always @(posedge Bus.Clock)
 		if (Bus.Reset) TWordCount <= '0;
-		else if (Load && SEL && FF) TWordCount[15:8] <= TWCIn[15:8];	
+		else if (Load && SEL && FF) TWordCount[15:8] <= TWCIn[7:0];	
 		else if (Load && SEL)       TWordCount[7:0]  <= TWCIn[7:0];
 		else if (Load)				TWordCount       <= TWCIn;
+		else 						TWordCount		 <= TWordCount;
 				  
 endmodule
 
@@ -172,11 +177,11 @@ module BaseAddressReg ( IntBus.BARMod Bus,  		//Input from Internal Data Bus
 
 	logic [15:0] BAddress;
 
-
 	always_ff @(posedge Bus.Clock)
 		if (Bus.Reset) BAddress <= '0;
 		else if (Load && FF) BAddress[15:8] <= Bus.Data;
 		else if (Load) 		 BAddress[7:0]  <= Bus.Data;
+		else 				 BAddress       <= BAddress;
 				   
 endmodule
 
@@ -193,6 +198,7 @@ module BaseWordCountReg ( IntBus.BWCMod Bus, 		 //Input from Internal Data Bus
 		if (Bus.Reset) BWordCount <= '0;
 		else if (Load && FF) BWordCount[15:8] <= Bus.Data;
 		else if (Load) 		 BWordCount[7:0]  <= Bus.Data;
+		else 				 BWordCount       <= BWordCount;
 
 endmodule
 
@@ -201,7 +207,7 @@ endmodule
 //Command Register
 //*********LIST BITS********
 module CommandReg(IntBus.CommandMod Bus, 					//Input from Internal Data Bus
-			      output logic CmdOut,						//Output to Priority and Control Blocks
+			      output logic [7:0] CmdOut,						//Output to Priority and Control Blocks
 			      input  logic Enable, Load, MasterClear);  
 
 	localparam DISABLE = 8'b0000_0100; //Bit pattern for controller disable
@@ -231,6 +237,7 @@ module ModeReg( IntBus.ModeMod Bus, 		 //Input from Internal Data Bus
 	always_ff @(posedge Bus.Clock)
 		if (Bus.Reset) Mode <= '0;
 		else if (Load) Mode <= Bus.Data[7:2];
+		else           Mode <= Mode;
 
 endmodule
 
@@ -241,7 +248,7 @@ endmodule
 //*********LIST BITS********
 module MaskReg ( IntBus.MaskMod Bus,				//Input from internal Data Bus
 				 output logic [3:0] MaskOut,		//Output to Priority Block
-				 input  ClearMask, SetAllMask, 		//Signals to Clear/Set All bits 
+				 input  ClearMask,  WriteAllMask,		        //Signals to Clear/Set All bits 
 				 input  logic Load, MasterClear);
 
 	logic [3:0] Mask;
@@ -249,9 +256,10 @@ module MaskReg ( IntBus.MaskMod Bus,				//Input from internal Data Bus
 	assign MaskOut = Mask;
 
 	always_ff @(posedge Bus.Clock)
-		if (Bus.Reset || MasterClear || SetAllMask)  Mask <= '1;	//Masks are Set on Reset or Master Clear
+		if (Bus.Reset || MasterClear || WriteAllMask)  Mask <= '1;	//Masks are Set on Reset or Master Clear
 		else if (ClearMask) Mask <= '0;	//Clear Mask Command Clears all Masks
 		else if (Load)      Mask <= Bus.Data;
+		else                Mask <= Mask;
 		
 	
 endmodule
@@ -265,7 +273,7 @@ endmodule
 //The Status is Reset upon begin Read by the CPU. 
 //*********LIST BITS********
 module StatusReg( IntBus.StatusMod Bus,					//Output to Internal Data Bus
-				  input PendingReq, TerminalCount, 	
+				  input [3:0] PendingReq, TerminalCount, 	
 				  input logic Enable, MasterClear);
 
 	logic [7:0] Status;
@@ -316,11 +324,13 @@ endmodule
 //Multiplexers
 module Mux2to1 #( parameter SIZE = 16) 
   				( output logic [SIZE-1:0] Out, 
-				  input  logic [SIZE>>1 -1:0] In1,
+				  input  logic [(SIZE>>1)-1:0] In1,
                   input  logic [SIZE-1:0] In2, 
                   input  logic Select);
 
-	assign Out = (Select) ? In1 : In2;
+    localparam HALF = SIZE >> 1;
+
+	assign Out = (Select) ? { {HALF{1'b1}}, In1} : In2; //extend In1 to be SIZE bits to match output size
 
 endmodule
 
@@ -343,7 +353,8 @@ endmodule
 //Internal bus interface with modports for Registers
 interface IntBus (input logic Clock, input logic Reset);
 	
-	wire [7:0]  Data;
+	//wire [7:0]  Data;
+	logic [7:0] Data;
 	
 	///*****CHANGE TO WRITE MODPORT
 	//Command Register
@@ -382,72 +393,4 @@ interface IntBus (input logic Clock, input logic Reset);
 	//Status Register				   
 	modport StatusMod  ( output Data, input Clock, Reset);
 
-endinterface
-
-
-
-//Datapath Control interface. Load and Enable for each Registers
-//as well as Master Clear. FF controls Upper or Lower Byte select on 
-//Some registers to be output to external Data Line DB7-DB0;
-interface ControlIF;
-
-	logic [3:0] ModeLoad;  	  					  //Mode Register 
-	logic [3:0] BARLoad; 		 				  //Base Address Registers
-	logic [3:0] CAREnable,   	 CARLoad;  		  //Current Address Registers
-	logic [3:0] TAREnbale,   	 TARLoad;  		  //Temporary Address Registers 
-	logic [3:0] BWCEnable, 		 BWCLoad; 		  //Base Word Count Registers 
-	logic [3:0] CWCEnable,       CWCLoad; 		  //Current Word Count Registers
-	logic [3:0] TWCEnable, 		 TWCLoad; 		  //Temporary Word Count Registers
-	logic [3:0] AddrMuxSel, 	 WCMuxSel; 		  //Select lines to Address and Word Count Multiplexors
-	logic 		CommandEnable, 	 CommandLoad;	  //Command Register
-	logic 		MaskLoad; 	          		  //Mask Register
-	logic 		StatusEnable;   				  //Status Register
-	logic 		AddrBufEnbable;					  //Address Buffer
-	logic 		DataBufEnable; 				  //Data Buffer Enable
-	logic 		DataBufRead, DataBufWrite;	  //Data Buffer Read/Write	
-	logic		ClearMask,    WriteAllMask;       //Clear and Set mask Registers
-	logic 		MasterClear,     FF ;
-	
-
-
-  //Modport For Timing Control Logic Block
-  modport TCL (
-	output ModeLoad,   
-	output BARLoad,  
-	output CAREnable,   	CARLoad,  
-	output TAREnbale,   	TARLoad,  
-	output BWCEnable, 		BWCLoad, 
-	output CWCEnable, 		CWCLoad, 
-	output TWCEnable, 		TWCLoad, 
-	output AddrMuxSel, 	    WCMuxSel,
-	output CommandEnable, 	CommandLoad, 
-	output MaskLoad, 	        
-	output StatusEnable,   
-	output AddrBufEnbable, 
-	output DataBufEnable,
-    output DataBufRead, DataBufWrite,	
-	output ClearMask,    WriteAllMask,
-	output MasterClear,     FF
-	);
-
-
-  //Modport for Datapath
-  modport Datapath (  
-	input ModeLoad,   
-	input BARLoad,  
-	input CAREnable,   	   CARLoad,  
-	input TAREnbale,   	   TARLoad,  
-	input BWCEnable, 	   BWCLoad, 
-	input CWCEnable, 	   CWCLoad, 
-	input TWCEnable, 	   TWCLoad, 
-	input AddrMuxSel, 	   WCMuxSel,
-	input CommandEnable,   CommandLoad, 
-	input MaskLoad, 	      
-	input StatusEnable,   
-	input AddrBufEnbable,  
-    input DataBufRead, DataBufWrite,
-	input ClearMask,    WriteAllMask,
-	input MasterClear,     FF 
-	);
-	
 endinterface
