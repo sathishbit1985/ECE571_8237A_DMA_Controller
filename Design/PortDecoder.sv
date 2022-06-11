1eff69ef30573ef292fb2b85ff9f7b8253472cea
Aniruddh1124 <93413662+Aniruddh1124@users.noreply.github.com>
2022-06-10 20:16:15 -0700

    Delete PortDecoder.sv

--- Design/PortDecoder.sv
+++
@@ -1,5 +0,0 @@
-module PortDecoder (SystemBusIF.PortDecoder PortDecoder, output logic nCS);
-	localparam DMA_ID = 4'hF;
-	
-	assign nCS = ((PortDecoder.Address[7:4] == DMA_ID) && (~PortDecoder.nIOW||~PortDecoder.nIOR)) ? 1'b0 : 1'b1;
-endmodule
