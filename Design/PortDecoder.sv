7090904fef3fbd0f58684667a899c8f02251de19
Aniruddh1124 <93413662+Aniruddh1124@users.noreply.github.com>
2022-06-10 21:51:05 -0700

    Update PortDecoder.sv

--- Design/PortDecoder.sv
+++ Design/PortDecoder.sv
@@ -1,5 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Designer Name: Ramesh Chandar Aniruddh Punnai
+// Module Name: Port Decoder
+// Project Name: DMA Controller
+//////////////////////////////////////////////////////////////////////////////////
+
 module PortDecoder (SystemBusIF.PortDecoder PortDecoder, output logic nCS);
-	localparam DMA_ID = 4'hF;
+	localparam DMA_ID = 12'hFFF;
 	
-	assign nCS = ((PortDecoder.Address[7:4] == DMA_ID) && (~PortDecoder.nIOW||~PortDecoder.nIOR)) ? 1'b0 : 1'b1;
+	assign nCS = ((PortDecoder.Address[15:4] == DMA_ID) && (~PortDecoder.nIOW||~PortDecoder.nIOR)) ? 1'b0 : 1'b1;
 endmodule
